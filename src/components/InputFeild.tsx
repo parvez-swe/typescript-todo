0def23d6a3a4b20040d92e989a8f69469d8b2d6d
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-09 20:28:33 +0600

    styling

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+// import 'style.css'
 const InputFeild = () => {
   return (
-    <div>InputFeild</div>
+    <form className='input'>
+        <input type='input' placeholder='Enter a task' className='input__box' />
+            <button className='input__submit'>Go</button>
+        
+    </form>
   )
 }
 
