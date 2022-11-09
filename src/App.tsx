5e0742fe2aa9899eada1723d674d1bf1e81f55c1
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-09 20:19:47 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <span className="heading">Taskify</span>
     </div>
   );
-}
+};
 
 export default App;
